204a16f742c36d70553437fadd37a02c1bcaba71
Grace Frank <frankgracey@gmail.com>
2018-12-05 13:42:17 +0100

    made changes to my function name

--- Grace Frank/KaprekarsConstant.js
+++ Grace Frank/KaprekarsConstant.js
@@ -1,8 +1,8 @@
-function KaprekarsConstant(){
+function kaprekarsConstant(){
 
     //code goes here
-    
+
 
 }
 
-module.exports = KaprekarsConstant;
+module.exports = kaprekarsConstant;
--- Grace Frank/KaprekarsConstant.test.js
+++ Grace Frank/KaprekarsConstant.test.js
@@ -1,11 +1,11 @@
-const KaprekarsConstant = require('./KaprekarsConstant');
+const kaprekarsConstant = require('./KaprekarsConstant');
 
 test('that function returns 3 when it takes  3524 as parameter', ()=>{
-    expect(KaprekarsConstant(3524)).toBe(3);
+    expect(kaprekarsConstant(3524)).toBe(3);
 });
 
 test('that function returns 5 when it takes  2111 as parameter', ()=>{
-    expect(KaprekarsConstant(2111)).toBe(5);
+    expect(kaprekarsConstant(2111)).toBe(5);
 });
 
 test('that function returns 6 when it takes 8 as parameter', ()=>{
@@ -13,32 +13,32 @@ test('that function returns 6 when it takes 8 as parameter', ()=>{
 });
 
 test('that function returns 5 when it takes  10 as parameter', ()=>{
-    expect(KaprekarsConstant(10)).toBe(5);
+    expect(kaprekarsConstant(10)).toBe(5);
 });
 
 test('that function returns 4 when it takes  0004 as parameter', ()=>{
-    expect(KaprekarsConstant(0004)).toBe(4);
+    expect(kaprekarsConstant(0004)).toBe(4);
 });
 
 test('that function returns 5 when it takes  0330 as parameter', ()=>{
-    expect(KaprekarsConstant(0330)).toBe(5);
+    expect(kaprekarsConstant(0330)).toBe(5);
 });
 test('that function returns 7 when it takes 9831 as parameter', ()=>{
-    expect(KaprekarsConstant(9831)).toBe(7);
+    expect(kaprekarsConstant(9831)).toBe(7);
 });
 
 test('that function returns 3 when it takes  7060 as parameter', ()=>{
-    expect(KaprekarsConstant(7060)).toBe(3);
+    expect(kaprekarsConstant(7060)).toBe(3);
 });
 
 test('that function returns 4 when it takes  6677 as parameter', ()=>{
-    expect(KaprekarsConstant(6677)).toBe(4);
+    expect(kaprekarsConstant(6677)).toBe(4);
 });
 
 test('that function returns 3 when it takes  2345 as parameter', ()=>{
-    expect(KaprekarsConstant(2345)).toBe(3);
+    expect(kaprekarsConstant(2345)).toBe(3);
 });
 
 test('that function returns 3 when it takes  5432 as parameter', ()=>{
-    expect(KaprekarsConstant(5432)).toBe(3);
+    expect(kaprekarsConstant(5432)).toBe(3);
 });

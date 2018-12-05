b915f3a90e8c4557302a37ea57c19423a06b6d69
Dare Lawal <lawaldare@gmail.com>
2018-12-05 09:11:33 +0100

    edited the solution file

--- removeVowels/removeVowels.js
+++ removeVowels/removeVowels.js
@@ -1,15 +1,6 @@
 
 function removeVowels(str){
 
-	let strSplitted = str.split('');
-    let vowel = ['a', 'e', 'i', 'o', 'u'];
-    let output = [];
-    for(let element of strSplitted){
-        if (!vowel.includes(element)){
-            output.push(element);
-        }
-    }
-    return output.sort().join('');
 	
 	
 }
